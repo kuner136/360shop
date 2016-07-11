6f931aaeb78f9b15bd85e10429cd4b41fcd687dc
kuner136 <870498684@qq.com>
2016-07-11 09:43:56 +0800

    add

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 var swiper = new Swiper('.swiper-container1', {
     pagination: '.swiper-pagination',
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
     paginationClickable: true,
     centeredSlides: true,
     loop : true,
@@ -10,7 +8,6 @@ var swiper = new Swiper('.swiper-container1', {
 });
 
 var swiper = new Swiper('.swiper-container2', {
-    pagination: '.swiper-pagination',
     slidesPerView: 3,
     slidesPerGroup:3,
 });
